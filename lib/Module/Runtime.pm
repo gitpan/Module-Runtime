05473b5dc6de0fa8f7be5eebaedbedb1569539f9
Andrew Main (Zefram) <zefram@fysh.org>
2004-10-29 19:39:29 +0000

    Import of ZEFRAM/Module-Runtime-0.001 from CPAN.
    gitpan-cpan-distribution: Module-Runtime
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         ZEFRAM/Module-Runtime-0.001.tar.gz
    gitpan-cpan-author:       ZEFRAM
    gitpan-cpan-maturity:     released

--- lib/Module/Runtime.pm
+++ lib/Module/Runtime.pm
@@ -5,14 +5,17 @@ Module::Runtime - runtime module handling
 =head1 SYNOPSIS
 
 	use Module::Runtime qw(is_valid_module_name require_module
+			use_module
 			is_valid_module_spec compose_module_name);
 
-	$ok = is_valid_module_name($ARGV[0]);
-	require_module($ARGV[0]);
+	$ok = is_valid_module_name($module);
+	require_module($module);
 
-	$ok = is_valid_module_spec("Standard::Prefix", $ARGV[0]);
+	$bi = use_module("Math::BigInt", 1.31)->new("1_234");
+
+	$ok = is_valid_module_spec("Standard::Prefix", $spec);
 	$module_name = compose_module_name("Standard::Prefix",
-						$ARGV[0]);
+						$spec);
 
 =head1 DESCRIPTION
 
@@ -29,12 +32,13 @@ use strict;
 use Carp qw(croak);
 use Exporter;
 
-our $VERSION = "0.000";
+our $VERSION = "0.001";
 
 our @ISA = qw(Exporter);
 
 our @EXPORT_OK = qw(
 	is_valid_module_name require_module
+	use_module
 	is_valid_module_spec compose_module_name
 );
 
@@ -42,7 +46,7 @@ our @EXPORT_OK = qw(
 
 =over
 
-=item C<< is_valid_module_name(I<STRING>) >>
+=item is_valid_module_name(STRING)
 
 This tests whether a string is a valid Perl module name, i.e., has valid
 bareword syntax.  The rule for this, precisely, is: the string must
@@ -61,17 +65,21 @@ sub is_valid_module_name($) {
 	$string =~ m#\A[a-zA-Z_]\w*(?:::\w+)*\z#
 }
 
-=item C<< require_module(I<NAME>) >>
+=item require_module(NAME)
 
 This is essentially the bareword form of C<require>, in runtime form.
-The I<NAME> is a string, which should be a valid module name (one or
+The NAME is a string, which should be a valid module name (one or
 more C<::>-separated segments).  If it is not a valid name, the function
 C<die>s.
 
-The module specified by I<NAME> is loaded, if it hasn't been already,
+The module specified by NAME is loaded, if it hasn't been already,
 in the manner of the bareword form of C<require>.  That means that a
 search through C<@INC> is performed, and a byte-compiled form of the
 module will be used if available.
+
+The return value is as for C<require>.  That is, it is the value returned
+by the module itself if the module is loaded anew, or 1 if the module
+was already loaded.
 
 =cut
 
@@ -81,12 +89,40 @@ sub require_module($) {
 	eval("require ".$name) || die $@;
 }
 
-=item C<< is_valid_module_spec(I<PREFIX>, I<SPEC>) >>
+=item use_module(NAME[, VERSION)
 
-Tests whether I<SPEC> is valid input for C<compose_module_name()>.
-See below for what that entails.  Whether a I<PREFIX> is supplied affects
-the validity of I<SPEC>, but the exact value of the prefix is unimportant,
-so this function treats I<PREFIX> as a boolean.
+This is essentially C<use> in runtime form, but without the "import"
+feature (which is fundamentally a compile-time thing).  The NAME is
+handled just like in C<require_module> above: it must be a module name,
+and the named module is loaded as if by the bareword form of C<require>.
+
+If a VERSION is specified, the "VERSION" method of the loaded module is
+called with the specified VERSION as an argument.  This normally serves to
+ensure that the version loaded is at least the version required.  This is
+the same functionality provided by the VERSION parameter of C<use>.
+
+On success, the name of the module is returned.  This is unlike
+C<require_module>, and is done so that the entire call to C<use_module>
+can be used as a class name to call a constructor, as in the example in
+the synopsis.
+
+=cut
+
+sub use_module($;$) {
+	my($name, $version) = @_;
+	require_module($name);
+	if(defined $version) {
+		$name->VERSION($version);
+	}
+	return $name;
+}
+
+=item is_valid_module_spec(PREFIX, SPEC)
+
+Tests whether SPEC is valid input for C<compose_module_name()>.
+See below for what that entails.  Whether a PREFIX is supplied affects
+the validity of SPEC, but the exact value of the prefix is unimportant,
+so this function treats PREFIX as a boolean.
 
 =cut
 
@@ -96,23 +132,23 @@ sub is_valid_module_spec($$) {
 		|| $spec =~ m{\A(?:/|::)?([a-zA-Z_]\w*(?:(?:/|::)\w+)*)\z};
 }
 
-=item C<< compose_module_name(I<PREFIX>, I<SPEC>) >>
+=item compose_module_name(PREFIX, SPEC)
 
 This function is intended to make it more convenient for a user to specify
 a Perl module name at runtime.  Users have greater need for abbreviations
 and context-sensitivity than programmers, and Perl module names get a
-little unwieldy.  I<SPEC> is what the user specifies, and this function
+little unwieldy.  SPEC is what the user specifies, and this function
 translates it into a module name in standard form, which it returns.
 
-I<SPEC> has syntax approximately that of a standard module name: it
+SPEC has syntax approximately that of a standard module name: it
 should consist of one or more name segments, each of which consists
 of one or more identifier characters.  However, C</> is permitted as a
 separator, in addition to the standard C<::>.  The two separators are
 entirely interchangeable.
 
-Additionally, if I<PREFIX> is not C<undef> then it must be a module
+Additionally, if PREFIX is not C<undef> then it must be a module
 name in standard form, and it is prefixed to the user-specified name.
-The user can inhibit the prefix addition by starting I<SPEC> with a
+The user can inhibit the prefix addition by starting SPEC with a
 separator (either C</> or C<::>).
 
 =cut
@@ -136,7 +172,8 @@ sub compose_module_name($$) {
 
 =head1 SEE ALSO
 
-L<perlfunc/require>
+L<perlfunc/require>,
+L<perlfunc/use>
 
 =head1 AUTHOR
 
